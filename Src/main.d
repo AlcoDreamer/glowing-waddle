584492a0ed8d1a52db2b79c500d21c296e0bf46d
AlvoDreamer <pyshbol@gmail.com>
2017-08-30 21:30:55 +0300

    smt

--- Src/main.d
+++
@@ -1,3 +0,0 @@
-/Users/dchernyshev/Documents/code/glowing-waddle/Src/main.o: \
-  /Users/dchernyshev/Documents/code/glowing-waddle/Src/main.cpp \
-  /Users/dchernyshev/Documents/code/glowing-waddle/Headers/game.h
